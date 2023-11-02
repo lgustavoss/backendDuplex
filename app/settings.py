4fe99a55993dc0b5f539e42a09e94739950bc9df
Luis Gustavo <luis_gustavope@icloud.com>
2023-11-02 17:28:25 -0300

    Adicionando a paginação dinamica no app Pessoa

--- app/settings.py
+++ app/settings.py
@@ -135,6 +135,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # Defina o número de itens por página usado como global no projeto.
 }
 
 
--- pessoa/views.py
+++ pessoa/views.py
@@ -1,11 +1,25 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from .models import Pessoa
 from .serializers import PessoaSerializer, PessoaListSerializer
 
 class PessoaViewSet(viewsets.ModelViewSet):
     queryset = Pessoa.objects.all()
+    serializer_class = PessoaSerializer
+    pagination_class = PageNumberPagination 
+
+    def list(self, request, *args, **kwargs):
+        # Acesse o valor de 'page_size' na consulta
+        page_size = request.query_params.get('page_size')
+        
+        if page_size:
+            # Se 'page_size' foi especificado, use o valor fornecido
+            self.paginator.page_size = int(page_size)
+
+        return super().list(request, *args, **kwargs)
 
     def get_serializer_class(self):
         if self.action == 'list':
             return PessoaListSerializer
         return PessoaSerializer
+
